75874b247db663b6f01c6d4e896fbd5ff9ed54d8
David Benoit <benoid@users.noreply.github.com>
2017-01-24 14:03:34 -0500

    Update map-star.scm

--- map-star.scm
+++ map-star.scm
@@ -3,7 +3,7 @@
 ;; Unleash the full power of mapping variardic functions.
 ;;
 ;; Scheme's built in map procedure operates on an arbitrary number
-;; of equal length lists.  This is neither a problem when mapping 
+;; of equal length lists.  This is not a problem when mapping 
 ;; with fixed-argument functions, or mapping variardic functions
 ;; over an explicit number of lists.  
 ;;
